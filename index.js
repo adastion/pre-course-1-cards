b38bd305fad5005feb97cbe908f2e92b42b85ef0
Andrey Sery <andrey.72006@gmail.com>
2023-11-26 18:38:00 +0300

    create card_block

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { getPathData, getMonths, getUserData } from './src/data.js'
-import { createMyElement} from './src/create_elements.js'
+import { createMyElement } from './src/create_elements.js'
+import { renderCard } from './src/components/card_block.js'
 
 // variables
 const pathPictures = getPathData()
@@ -12,64 +13,23 @@ const header = createMyElement("header", rootElement, "header")
 const container = createMyElement("div", header, "container")
 const logo = createMyElement("a", container, "logo", "", "", "#")
 const logoImage = createMyElement("img", logo, "", "", pathPictures.logo)
-logoImage.alt = logo
+  logoImage.alt = logo
 const greeting = createMyElement("h1", container, "",  `Welcome Back, ${userData.nameUser}!`)
 const contentCards = createMyElement("div", rootElement, "container")
 const titleCards = createMyElement("h2", contentCards, "", "My Cards")
 
 //rendering cards list
 const cardsList = createMyElement("ul", contentCards, "cards")
-for (let i = 0; i < userData.userCards.length; i++) {
-  const cardData = userData.userCards[i]
-  //  renderCrad(cardData, cardsList)
-  
-    const cardItem = createMyElement("li", cardsList, "card")
-    const cardContent = createMyElement("section", cardItem, "card__content")
-    let colorCard = "";
-    let currencySign = "";
-    
-    switch (cardData.currencyType) {
-      case "USD":
-        currencySign = "$";
-        colorCard = "linear-gradient(-175deg, #9C2CF3, #3A6FF9)";
-        break;
-        case "EUR":
-          currencySign = "€";
-          colorCard = "linear-gradient(-175deg, #F32CC7, #27A6AE)";
-          break;
-          case "GBP":
-            currencySign = "£";
-            colorCard = "linear-gradient(-175deg, #F3972C, #E333C7)";
-            break;
-          }
-          const card = createMyElement("div", cardContent, "card__card")
-          card.style.background = colorCard
-}
+  for (let i = 0; i < userData.userCards.length; i++) {
+    const cardData = userData.userCards[i]
+    renderCard(cardData, cardsList, pathPictures)
+  }
 
-//             document.write(`<div class='card__card' style='background: ${colorCard};'>`);
-//               document.write(`<div class='card__item'>`);
-//                 document.write(`<h3>${card.type} card</h3>`);
-//                 document.write(`<p>Current Balance</p>`);
-//                 document.write(`<h4>${currencySign}${card.currentBalance}</h4>`);
-//                 document.write(`<p>${card.cardNumber} </p>`);
-//               document.write(`</div>`);
-//               document.write(`<div class='card__item'>`);
-//                 document.write(`<img src=${card.networkType === "Mastercard" ? pathPictures.master : pathPictures.visa} alt=logo-card-network-type/>`);
-//                 document.write(`<span >${card.expirationMonth} / ${card.expirationYear % 100}</span>`);
-//               document.write(`</div>`);
-//             document.write(`</div>`);
 
-//             document.write(`<div class='padlock'>
-//                             ${card.isActive 
-//                               ? '<img src="src/assets/images/icon-padlock.svg" alt="icon"/>' 
-//                               + "<span>Card is temporarily blocked</span>" 
-//                               : ""}
-//                             </div>`);
-//             document.write(`<h4 class='transaction-title'>History Transactions</h4>`);
-//             document.write(`<ul class='transaction'>`);
+//               document.write(`<h4 class='transaction-title'>History Transactions</h4>`);
+//               document.write(`<ul class='transaction'>`);
 //                 for (let numTransaction = 0; numTransaction < card.historyTransactions.length; numTransaction++) {
 //                   let transaction = card.historyTransactions[numTransaction];
-
 //                   document.write(`<li class='transaction__item'>`);
 //                     let iconPositiveDebiting =
 //                       transaction.amount > 0
---
+++ src/components/card_block.js
@@ -0,0 +1,40 @@
+import { createMyElement } from "../create_elements.js"
+import { renderBlockedCard } from "./renderBlockedCard.js"
+
+export function renderCard (cardData, cardsList, pathPictures) {
+  const cardBlock = createMyElement("li", cardsList, "card")
+  const cardContent = createMyElement("section", cardBlock, "card__content")
+  let colorCard;
+  let currencySign;
+  
+  // switchCase()
+
+  switch (cardData.currencyType) {
+    case "USD":
+      currencySign = "$";
+      colorCard = "bg-1";
+      break;
+      case "EUR":
+        currencySign = "€";
+        colorCard = "bg-2";
+        break;
+        case "GBP":
+          currencySign = "£";
+          colorCard = "bg-3";
+          break;
+        }
+  // card
+  const card = createMyElement("div", cardContent, "card__card")
+    card.classList.add(colorCard)
+  const cardItem = createMyElement("div", card, "card__item")
+  const titleCardType = createMyElement("h3", cardItem, "", `${cardData.type} card`)
+  const titleBalance = createMyElement("p", cardItem, "", "Current Balance")
+  const balance = createMyElement("h4", cardItem, "", `${currencySign}${cardData.currentBalance}`)
+  const cardNumber = createMyElement("p", cardItem, "", cardData.cardNumber)
+  const cardItem2 = createMyElement("div", card, "card__item")
+  const networkTypeLogo = createMyElement("img", cardItem2, "", "", `${cardData.networkType === "Mastercard" ? pathPictures.master : pathPictures.visa}`)
+    networkTypeLogo.alt = "logo-card-network-type"
+  const cardDate = createMyElement("span", cardItem2, "", `${cardData.expirationMonth} / ${cardData.expirationYear % 100}`)
+
+  renderBlockedCard(cardData, cardContent)
+}
